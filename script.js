b22eb15702cebd4233694e0dbcb7bd5f7b25bbcc
YounessErrid <younesserrid2@gmail.com>
2022-02-09 23:35:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const dd  =   document.getElementById("days");
+const hh =   document.getElementById("hours");
+const mm  =   document.getElementById("mins");
+const ss   =   document.getElementById("seconds");
+const yy = new Date();
+document.getElementById("year").innerHTML = yy.getFullYear();
+
+
+function countdown(){
+    const curDate = new Date();
+    const newYearsDate = new Date("1 jan " + (curDate.getFullYear() +1) );
+    const Tseconds = (newYearsDate - curDate) / 1000;
+    
+    const days = Math.floor(Tseconds / 86400);
+    const hours = Math.floor(Tseconds / 3600) % 24;
+    const mins = Math.floor(Tseconds / 60) % 60;
+    const seconds = Math.floor(Tseconds) % 60;
+    
+    dd.innerHTML = days;
+    hh.innerHTML = hours;
+    mm.innerHTML = mins;
+    ss.innerHTML = seconds;
+}
+
+countdown();
+setInterval(countdown , 1000);
